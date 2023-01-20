eaa782750167278ed284da543eac06a3476e994c
Rodrigo Olivares <rodrigo.olivares@basedos.cl>
2023-01-20 11:01:15 -0300

    husky re-enabled

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import NumToTextConverter from './num-to-text-es'
 import { ICaseTransform } from './transforms/case-transform'
 import LowerCaseTransform from './transforms/lower-case-transform'
 import TitleCaseTransform from './transforms/title-case-transform'
-import UpperCaseTransform from './transforms/upper-case-transform';
+import UpperCaseTransform from './transforms/upper-case-transform'
 import * as Types from './types'
 
 const transforms: ICaseTransform[] = [
