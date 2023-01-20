dbd318928e760140f1dbf00fa95bb0d579927639
Rodrigo Olivares <rodrigo.olivares@basedos.cl>
2023-01-20 09:16:46 -0300

    v0.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import NumToTextConverter from './num-to-text-es'
+import { ICaseTransform } from './transforms/case-transform'
+import LowerCaseTransform from './transforms/lower-case-transform'
+import TitleCaseTransform from './transforms/title-case-transform'
+import UpperCaseTransform from './transforms/upper-case-transform'
 import * as Types from './types'
 
+const transforms: ICaseTransform[] = [
+  new LowerCaseTransform(),
+  new UpperCaseTransform(),
+  new TitleCaseTransform()
+]
+
+const defaultConverter = new NumToTextConverter(transforms)
+
 export {
+  defaultConverter as numToText,
   NumToTextConverter,
   Types
 }
